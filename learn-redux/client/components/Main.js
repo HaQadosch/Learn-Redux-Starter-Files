98b142ddb4a41188022a5de92f5f432e7cb8a6fb
HaQadosch Berraka <haqadosch@pm.me>
2019-03-05 22:05:02 +0000

    Lesson 2

---
+++ learn-redux/client/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+export class Main extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1><Link to='/'>ReduxStagram</Link></h1>
+      </div>
+    )
+  }
+}
