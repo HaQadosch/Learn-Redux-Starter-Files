d9edb36573040f604cdb1f9e1f3fc8ed757dd9a6
HaQadosch Berraka <haqadosch@pm.me>
2019-03-06 21:20:55 +0000

    Lesson 3: using Switch instead of IndexRoute.

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Main } from './components/Main'
+import { PhotoGrid } from './components/PhotoGrid'
+import { Single } from './components/Single'
 import css from './styles/style.styl'
 
-render(<Main />, document.querySelector('#root'))
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+
+const route = (
+  <Router history={browserHistory} >
+    <Route path='/' component={Main} >
+      <IndexRoute component={PhotoGrid} />
+      <Route path='/view/:postId' component={Single} />
+    </Route>
+  </Router>
+)
+
+render(route, document.querySelector('#root'))
