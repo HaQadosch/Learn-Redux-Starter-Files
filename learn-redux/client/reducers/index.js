55af55756072273fb5c50238aa548192f614e976
HaQadosch Berraka <haqadosch@pm.me>
2019-03-11 15:51:36 +0000

    Lesson 8

--- learn-redux/client/reducers/index.js
+++ learn-redux/client/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
+import { routerReducer as routing } from 'react-router-redux'
 
 import { posts } from './posts'
 import { comments } from './comments'
 
-export const rootReducer = combineReducers({ posts, comments, router: routerReducer })
+export const rootReducer = combineReducers({ posts, comments, routing })
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -5,15 +5,20 @@ import { PhotoGrid } from './components/PhotoGrid'
 import { Single } from './components/Single'
 import css from './styles/style.styl'
 
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute } from 'react-router'
+
+import { Provider } from 'react-redux'
+import { store, history } from './store'
 
 const route = (
-  <Router history={browserHistory} >
-    <Route path='/' component={Main} >
-      <IndexRoute component={PhotoGrid} />
-      <Route path='/view/:postId' component={Single} />
-    </Route>
-  </Router>
+  <Provider store={store} >
+    <Router history={history} >
+      <Route path='/' component={Main} >
+        <IndexRoute component={PhotoGrid} />
+        <Route path='/view/:postId' component={Single} />
+      </Route>
+    </Router>
+  </Provider>
 )
 
 render(route, document.querySelector('#root'))
