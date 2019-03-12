d8b7f2f81e599747abc600759fb23dd3d4d50107
HaQadosch Berraka <haqadosch@pm.me>
2019-03-12 21:04:08 +0000

    Lesson 13

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import { Photo } from './Photo'
+import { Comments } from './Comments'
 
 export class Single extends Component {
   render () {
+    const { posts, params: { postId } } = this.props
+    const i = posts.findIndex(({ code }) => code === postId)
+    const post = posts[i]
+
     return (
-      <div className='single-photo'>Single</div>
+      <div className='single-photo'>
+        <Photo key={i} index={i} post={post} {...this.props} />
+        <Comments />
+      </div>
     )
   }
 }
